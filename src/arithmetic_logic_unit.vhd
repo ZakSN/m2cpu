e37ee05fd0364378662dc70c7881e766d975e313
Zakary Nafziger <worldofzak@gmail.com>
2017-10-12 19:25:35 -0400

    more complete alu, still untested

--- src/arithmetic_logic_unit.vhd
+++ src/arithmetic_logic_unit.vhd
@@ -3,44 +3,65 @@ use ieee.std_logic_1164.all;
 
 entity arithmetic_logic_unit is port
 (
-	x_arg  : in std_logic_vector(7 downto 0); --operand from x register
-	y_arg  : in std_logic_vector(7 downto 0); --operand from the y register
-	acc_in : in std_logic_vector(7 downto 0);
-	result : out std_logic_vector(7 downto 0); --result of operation between x and y
-	opr    : in std_logic_vector(2 downto 0); --the operation to perform
-	zro, neg, cry, ovf : out std_logic --zero, negative, carry, overflow flag inputs
+	xin : in std_logic_vector(7 downto 0); --operand from x register
+	yin : in std_logic_vector(7 downto 0); --operand from the y register
+	res : out std_logic_vector(7 downto 0); --result of operation between x and y
+	opr : in std_logic_vector(2 downto 0); --the operation to perform
+	zro, neg, cry, ovf : out std_logic --zero, negative, carry, overflow flag outputs
 ):
 end entity arithmetic_logic_unit;
 
 architecture a0 of aritmetic_logic_unit is
-
-	signal x   : unsigned(7 downto 0);
-	signal y   : unsigned(7 downto 0);
-	signal add : std_logic_vector(7 downto 0);
-	signal sub : std_logic_vector(7 downto 0);
+	
+	--9 bit collections to perserve carry bit
+	signal x_u9     : unsigned(8 downto 0);
+	signal y_u9     : unsigned(8 downto 0);
+	signal add_slv9 : std_logic_vector(8 downto 0);
+	signal sub_slv9 : std_logic_vector(8 downto 0);
+	
+	--8 bit output vector
+	signal add_slv8 : std_logic_vector(7 downto 0);
+	signal sub_slv8 : std_logic_vector(7 downto 0);
+	
+	signal ppn : std_logic;
+	signal nnp : std_logic;
 
 begin
    
-	--A/L logic:
-	x <= signed(x_arg);
-	y <= signed(y_arg);
-	add <= std_logic_vector(x + y);
-	sub <= std_logic_vector(x - y);
+	--------------artihmetic--------------
+	x_u9 <= signed('0' & xin);
+	y_u9 <= signed('0' & yin);
+	add_slv9 <= std_logic_vector(x_u9 + y_u9);
+	sub_slv9 <= std_logic_vector(x_u9 - y_u9);
+	add_slv8 <= add_slv9(7 downto 0);
+	sub_slv8 <= sub_slv9(7 downto 0);
 	
+	--------------select output--------------
 	with opr select
-		result <= add 							 when "000", --x + y
-					 sub 							 when "001", --x - y
-					 x_arg AND y_arg 			 when "010", --x AND y
-					 x_arg NAND y_arg 		 when "011", --x NAND y
-					 x_arg OR y_arg 			 when "100", --x OR y
-					 x_arg XOR y_arg         when "101", --x XOR y
-					 x_arg(6 downto 0) & '0' when "110", --shift x left (STL)
-					 '0' & x_arg(7 downto 1) when "111", --shift x rgiht (STR)
-					 "00000000" 				 when others;
-					
-	--flag set logic:
+		res <= add_slv8 when "000", --x + y
+				 sub_slv8 when "001", --x - y
+				 xin AND  yin when "010", --x AND y
+				 xin NAND yin when "011", --x NAND y
+				 xin OR   yin when "100", --x OR y
+				 xin XOR  yin when "101", --x XOR y
+				 xin(6 downto 0) & '0' when "110", --shift x left (STL)
+				 '0' & xin(7 downto 1) when "111", --shift x rgiht (STR)
+				 "00000000" when others;
+				 
+	--------------flag set logic--------------
 	with opr select
-		cry <= x_arg(7) when "110",
-				 x_arg(0) when "111",
+		cry <= add_slv9(8) when "000", --carry (addition)
+				 sub_slv9(8) when "001", --borrow (subtraction)
+				 xin(7) when "110", --leftmost bit of shift
+				 xin(0) when "111", --rightmost bit of shift
+				 "0" when others;
+
+	ppn <= NOT(xin(7)) AND NOT(yin(7)) AND res(7); --positive arguments, negative result
+	nnp <= xin(7) AND yin(7) AND NOT(res(7)); --negative arguments, positive result
+	ovf <= ppn OR nnp when opr="000" else '0'; --overflow only occurs during (signed) addition
+	
+	neg <= res(7); --sign of result
+	
+	zro <= '1' when res="00000000" else '0'; --result is zero
 				
 end architecture a0;
