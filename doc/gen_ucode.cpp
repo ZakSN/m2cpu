6bdf23de4f4f79a9fd9c3f3d6f5ad349d0c1c460
Zakary Nafziger <worldofzak@gmail.com>
2017-11-04 14:00:37 -0400

    made gen_ucode slightly less embarrassing

--- doc/gen_ucode.cpp
+++ doc/gen_ucode.cpp
@@ -2,25 +2,27 @@
 # include <stdint.h>
 using namespace std;
 
-int intpow(int, int);
+uint64_t intpow(uint64_t, int);
 
 int main(){
-	uint64_t micro_ins = 1;
+	uint64_t micro_ins = 0;
 	int bit_to_toggle = 0;
 	uint64_t bit_mask = 0;
-	while (bit_to_toggle >= 0 && bit_to_toggle < 48){
+	while (true){
 		cout<<"Enter number of bit to toggle"<<endl;
 		cin>>bit_to_toggle;
+		if (bit_to_toggle < 0 || bit_to_toggle > 48)
+			break;
 		bit_mask = intpow(2, bit_to_toggle);
 		micro_ins = micro_ins ^ bit_mask;
 	}
-	cout<<"microinstruction in unpadded hex is:"<<endl;
-	printf("%X\n", micro_ins);
+	cout<<"microinstruction in hex is:"<<endl;
+	printf("%.12llX\n", micro_ins);
 	return 0;
 }
 
-int intpow(int base, int exp){
-	int result = 1;
+uint64_t intpow(uint64_t base, int exp){
+	uint64_t result = 1;
 	for(int c=0; c<exp; c++){
 		result = result*base;
 	}
